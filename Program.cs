5b800b0d5bea6de2d1df955c2a94f77cd5827be1
AlexFerJesus <alexfernandesdejesus.12@gmail.com>
2023-10-27 14:45:50 -0300

    Adição de menu e forma de interação

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("QUADRANTE");
+Console.WriteLine("Digite um numero que indique um ponto");
+
+double X;
+double Y;
+
+Console.WriteLine("Ponto X:");
+X = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Ponto Y:");
+Y = Convert.ToDouble(Console.ReadLine());
+

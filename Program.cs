c10af2e8dcd45628ac01f77f023e8e3b2b870b7e
AlexFerJesus <alexfernandesdejesus.12@gmail.com>
2023-10-27 15:26:05 -0300

    Adição dos quadrantes

--- Program.cs
+++ Program.cs
@@ -9,3 +9,14 @@ X = Convert.ToDouble(Console.ReadLine());
 Console.WriteLine("Ponto Y:");
 Y = Convert.ToDouble(Console.ReadLine());
 
+if (X > 0 && Y > 0)
+    {Console.WriteLine($" O ponto ({X}, {Y}) esta no quadrante I.");}
+
+    else if (X < 0 && Y > 0)
+    {Console.WriteLine($" O ponto ({X}, {Y}) esta no quadrante II.");}
+
+    else if (X < 0 && Y < 0)
+    {Console.WriteLine($" O ponto ({X}, {Y}) esta no quadrante III.");}
+
+    else if (X > 0 && Y < 0) 
+    {Console.WriteLine($" O ponto ({X}, {Y}) esta no quadrante IV.");}
